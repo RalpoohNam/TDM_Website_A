97ceafd60d6a47b053ff19939f716671a825f114
Ralpooh.Nam <ehddna887@gmail.com>
2022-08-06 08:06:40 +0900

    chg mock<->kimp

--- kimpinfoapp/urls.py
+++ kimpinfoapp/urls.py
@@ -5,6 +5,8 @@ from kimpinfoapp.views import kimp_table, mockuppage
 app_name = "kimpinfoapp"
 
 urlpatterns = [
-    path('', mockuppage),
-    path('kimp_table/', kimp_table)
+    # path('', mockuppage),
+    # path('kimp_table/', kimp_table)
+    path('', kimp_table),
+    path('mockup/', mockuppage),
 ]
