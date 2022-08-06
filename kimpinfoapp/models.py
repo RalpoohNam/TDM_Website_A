a766286a7c64839c98bc0692d5efc18abf6a7203
Ralpooh.Nam <ehddna887@gmail.com>
2022-08-07 03:47:09 +0900

    test new brach

--- kimpinfoapp/models.py
+++ kimpinfoapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Request(models.Model):
+    baseExchange = models.CharField(max_length=50)
+    compExchage = models.CharField(max_length=50)
+
