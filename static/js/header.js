39081089267098e3a8c2d07127c29742251d8fd9
Ralpooh.Nam <ehddna887@gmail.com>
2022-08-07 09:14:08 +0900

    update disign

--- static/js/header.js
+++ static/js/header.js
@@ -1,10 +1,27 @@
-const toggleBtn = document.querySelector('.navbar__toggleBtn');
-const menu = document.querySelector('.navbar__menu');
-const icon = document.querySelector('.navbar__icon');
+const toggleBtn = document.querySelector('#menu__button');
+const insightBtn = document.querySelector('.insight__button');
+const marketinfoBtn = document.querySelector('.marketinfo__button');
 
-toggleBtn.addEventListener('click',()=>{
+const menu = document.querySelector('.main__menu');
+const icons = document.querySelector('.userinfoAndSetting');
+const insightmenu = document.querySelector('.insight__submenu');
+const marketinfomenu = document.querySelector('.marketinfo__submenu');
+
+toggleBtn.addEventListener('click',() => {
     menu.classList.toggle('active');
-    icon.classList.toggle('active');
+    icons.classList.toggle('active');
 });
 
+insightBtn.addEventListener('mouseover',() =>{
+    insightmenu.style.display='flex'
+});
+insightBtn.addEventListener('mouseout',() =>{
+    insightmenu.style.display='none';
+});
 
+marketinfoBtn.addEventListener('mouseover',() =>{
+    marketinfomenu.style.display='flex'
+});
+marketinfoBtn.addEventListener('mouseout',() =>{
+    marketinfomenu.style.display='none';
+});
